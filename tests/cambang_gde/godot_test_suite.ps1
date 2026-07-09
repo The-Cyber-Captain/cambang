42f007603d9b8115c5e72a742602c8ed01d9d534
The-Cyber-Captain <c.kerr@sixdegreesofcrispybacon.com>
2026-07-09 13:42:27 +0100

    Fix scene 70 test (timeout) in suite harness

--- tests/cambang_gde/godot_test_suite.ps1
+++ tests/cambang_gde/godot_test_suite.ps1
@@ -60,7 +60,7 @@ $SceneRuns = @(
     @{ Scene = "res://scenes/63_snapshot_observer_minimal.tscn";            QuitAfter = 10   },
     @{ Scene = "res://scenes/65_public_boundary_verify.tscn";               QuitAfter = 10   },
     @{ Scene = "res://scenes/66_public_lifecycle_verify.tscn";              QuitAfter = 1000 },
-    @{ Scene = "res://scenes/70_result_retrieval_verification.tscn";        QuitAfter = 20   }
+    @{ Scene = "res://scenes/70_result_retrieval_verification.tscn";        QuitAfter = 1000 }
 )
 
 # Optional custom success rules by step name.
